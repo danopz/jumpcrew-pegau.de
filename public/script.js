25573331ee84bf6cf640380c3d0e0b2ebb715181
Daniel Opitz <git@copynpaste.de>
2018-11-08 23:55:42 +0100

    move data to directory, merge api data into single json, jpg for profile images

--- public/script.js
+++ public/script.js
@@ -1,105 +1,78 @@
 var countup = function (from, to, time, cb) {
-    var steps = time / 50;
-    var incr = Math.ceil((to - from) / Math.floor(steps));
+        var steps = time / 50,
+            incr = Math.ceil((to - from) / Math.floor(steps)),
+            inter = setInterval(function () {
+                from += incr;
 
-    var inter = setInterval(function () {
-        from += incr;
+                if (from >= to) {
+                    from = to;
+                    clearInterval(inter);
+                }
 
-        if (from >= to) {
-            from = to;
-            clearInterval(inter);
-        }
+                cb(from);
+            }, time / steps);
+    },
+    sn = function (nameEle, followersEle, postsEle) {
+        var data = {
+            name: '',
+            followers: 0,
+            posts: 0
+        };
 
-        cb(from);
-    }, time / steps);
-};
+        this.setName = function (name) {
+            if (nameEle && name && data.name !== name) {
+                data.name = name;
+                nameEle.textContent = name;
+            }
+        };
+        this.setFollowers = function (followers) {
+            if (followersEle && followers && data.followers !== followers) {
+                data.followers = followers;
 
-var sn = function (nameEle, imageEle, followersEle, postsEle) {
-    var data = {
-        name: '',
-        followers: 0,
-        posts: 0,
-        image: ''
-    };
+                countup(0, followers, 1500, function (i) {
+                    followersEle.textContent = i;
+                })
+            }
+        };
+        this.setPosts = function (posts) {
+            if (postsEle && posts && data.posts !== posts) {
+                data.posts = posts;
 
-    this.setName = function (name) {
-        if (nameEle && name && data.name !== name) {
-            data.name = name;
-            nameEle.textContent = name;
-        }
-    };
-    this.setFollowers = function (followers) {
-        if (followersEle && followers && data.followers !== followers) {
-            data.followers = followers;
+                countup(0, posts, 2000, function (i) {
+                    postsEle.textContent = i;
+                })
+            }
+        };
+    },
+    fb = document.querySelector('.fb'),
+    insta = document.querySelector('.inst'),
+    yt = document.querySelector('.yt'),
+    fbd = new sn(
+        fb.querySelector('.name'),
+        fb.querySelector('.followers')
+    ),
+    ibd = new sn(
+        insta.querySelector('.name'),
+        insta.querySelector('.followers'),
+        insta.querySelector('.posts')
+    ),
+    ybd = new sn(
+        yt.querySelector('.name'),
+        yt.querySelector('.followers'),
+        yt.querySelector('.posts')
+    );
 
-            countup(0, followers, 1500, function (i) {
-                followersEle.textContent = i;
-            })
-        }
-    };
-    this.setPosts = function (posts) {
-        if (postsEle && posts && data.posts !== posts) {
-            data.posts = posts;
-
-            countup(0, posts, 2000, function (i) {
-                postsEle.textContent = i;
-            })
-        }
-    };
-    this.setImage = function (image) {
-        if (imageEle && image && data.image !== image) {
-            data.image = image;
-            imageEle.setAttribute('src', image);
-        }
-    };
-};
-
-var fb = document.querySelector('.fb');
-var insta = document.querySelector('.inst');
-var yt = document.querySelector('.yt');
-
-var fbd = new sn(
-    fb.querySelector('.name'),
-    fb.querySelector('.profile-img'),
-    fb.querySelector('.followers')
-);
-
-var ibd = new sn(
-    insta.querySelector('.name'),
-    insta.querySelector('.profile-img'),
-    insta.querySelector('.followers'),
-    insta.querySelector('.posts')
-);
-
-var ybd = new sn(
-    yt.querySelector('.name'),
-    yt.querySelector('.profile-img'),
-    yt.querySelector('.followers'),
-    yt.querySelector('.posts')
-);
-
-fetch('facebook.json').then(function (res) {
+fetch('api/data.json').then(function (res) {
     return res.json();
 }).then(function (data) {
-    fbd.setName(data.name);
-    fbd.setImage('data:' + data.image.mime + ';charset=utf8;base64,' + data.image.data);
-    fbd.setFollowers(data.followers);
-});
+    fbd.setName(data.facebook.name);
+    fbd.setFollowers(data.facebook.followers);
 
-fetch('instagram.json').then(function (res) {
-    return res.json();
-}).then(function (data) {
-    ibd.setName(data.name);
-    ibd.setImage('data:' + data.image.mime + ';charset=utf8;base64,' + data.image.data);
-    ibd.setFollowers(data.followers);
-    ibd.setPosts(data.posts);
-});
+    ibd.setName(data.instagram.name);
+    ibd.setFollowers(data.instagram.followers);
+    ibd.setPosts(data.instagram.posts);
 
-fetch('youtube.json').then(function (res) {
-    return res.json();
-}).then(function (data) {
-    ybd.setName(data.name);
-    ybd.setImage('data:' + data.image.mime + ';charset=utf8;base64,' + data.image.data);
-    ybd.setFollowers(data.followers);
-    ybd.setPosts(data.posts);
+    ybd.setName(data.youtube.name);
+    ybd.setFollowers(data.youtube.followers);
+    ybd.setPosts(data.youtube.posts);
 });
--- update.sh
+++ update.sh
@@ -15,11 +15,7 @@ done
 git pull
 [ ${CHANGES_COUNT} -eq 0 ] || git stash pop
 
-files=("public/facebook.json" "public/instagram.json" "public/youtube.json")
-
-for f in ${files[@]}; do
-    git add ${f}
-done
+git add public/api
 
 git commit -m"Automatic API data update"
 git push origin HEAD
